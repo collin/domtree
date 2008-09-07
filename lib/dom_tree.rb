3eb50021001c4fab57806695015a8369544442a5
collin <collintmiller@gmail.com>
2008-09-07 02:20:02 -0500

    generates bookmarklet

--- lib/dom_tree.rb
+++ lib/dom_tree.rb
@@ -5,6 +5,7 @@ Pathname.send :alias_method, '/', '+'
 DIR = Pathname.new("#{Pathname.new(__FILE__).dirname}/..")
 
 module DomTree
+  BookmarkletUrl = "http://localhost:4567/embedded.js"
   class << self
     def root
       DIR
@@ -13,5 +14,10 @@ module DomTree
     def vendor
       root/'vendor'
     end
+
+    def app
+      root/'app'
+    end
   end
 end
+
