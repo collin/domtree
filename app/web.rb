74de3ce0705c13e60cb0ee7b19cf3175ea920983
collin <collintmiller@gmail.com>
2008-09-07 01:45:21 -0500

    sinatra plays

--- app/web.rb
+++ app/web.rb
@@ -1,1 +1,23 @@
+require 'rubygems'
+require 'pathname'
+Pathname.send :alias_method, '/', '+'
 
+DIR = Pathname.new("#{Pathname.new(__FILE__).dirname}/..")
+
+module DomTree
+  class << self
+    def root
+      DIR
+    end
+    
+    def vendor
+      root/'vendor'
+    end
+  end
+end
+
+require DomTree.vendor/'sinatra'/'lib'/'sinatra'
+
+get '/' do
+  haml "%h1 EUREKA"
+end
